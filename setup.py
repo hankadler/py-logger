a29799185d1cccc90e14f65ca38b024004d434b1
Hank Adler <henryaguila1988@gmail.com>
2022-09-04 17:23:25 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+
+setup(
+    name='logger',
+    version='0.1.0',
+    description='Built-in logger with added functions',
+    author='Hank Adler',
+    packages=['logger'],
+)
